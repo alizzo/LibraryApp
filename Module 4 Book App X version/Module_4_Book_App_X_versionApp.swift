4db212b83246aa84f848cb7f655c4cbcedbc2b85
X <lizzo.alex@gmail.com>
2021-06-24 21:22:51 -0400

    Initial Commit

---
+++ Module 4 Book App X version/Module_4_Book_App_X_versionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module_4_Book_App_X_versionApp.swift
+//  Module 4 Book App X version
+//
+//  Created by X on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Module_4_Book_App_X_versionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
